ab63fa2ae685eade271a4aec8d819aeb96dfdf47
= <=>
2025-04-25 08:49:39 +0000

    Algo that multiply two numbers

---
+++ First_session/produit_de_nombres.py
@@ -0,0 +1,9 @@
+"""
+  Un algorithme qui permet de calculer le produit de deux nombres
+"""
+
+nbr_1 = float(input("Saisir la première variable : "))
+nbr_2 = float(input("Saisir la deuxième variable : "))
+result = nbr_1 * nbr_2
+
+print("Le résult du calcul est : ", result)
