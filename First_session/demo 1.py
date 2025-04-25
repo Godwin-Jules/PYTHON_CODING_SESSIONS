880a85e342a63aef5ced400f89b475c1019288c3
= <=>
2025-04-25 08:30:25 +0000

    First session in python programming

---
+++ First_session/demo 1.py
@@ -0,0 +1,41 @@
+""" Plan de cours
+      * Comment faire afficher un message à l'écran
+      * Comment écrire les commentaires
+      * les variables
+"""
+
+# Ceci c'est pour faire afficher un message à l'écran
+""" Tout ce qui va suivre
+c'est un example de commentaires 
+sur plusieurs lignes de code
+Est-ce que tu me comprends bien !"""
+
+# Comment faire échapper certains caractères à l'interpréteur et l'afficher à l'écran
+print('L\'élève est en retard')
+
+
+print("Hello world")
+nom = input("quel est votre nom? ")
+age = input("quel est votre age? ")
+print("mon nom est", nom, "je suis agé de", age, "ans")
+
+a = input("entrez la valeur ")
+print(type(a))
+a = int(a)
+print(type(a))
+if (a>0):
+  print(a, "maj")
+danger = "tu connais"
+danser = 655
+sourir = 45.45
+soit = '45'
+soit_converted = int(soit)
+soit_converted_2 = float(soit)
+
+print(type(danger))
+print(type(danser))
+print(type(sourir))
+print(type(soit))
+print(type(soit_converted))
+print(type(soit_converted_2))
+
