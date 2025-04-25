d62282b9f1a67889ea4fb2c017303d018f7fc7ee
= <=>
2025-04-25 08:41:53 +0000

    Algo that sums three numbers

---
+++ First_session/compute_numbers.py
@@ -0,0 +1,11 @@
+"""
+  un algo qui permet de calculer la somme de trois nombres entiers
+"""
+
+# Lecture des variables
+val_1 = int(input("Saisir la première variable : "))
+val_2 = int(input("Saisir la deuxième variable : "))
+val_3 = int(input("Saisir la troisième variable : "))
+result = val_1 + val_2 + val_3
+
+print("Le résultat du calcul est", result)
