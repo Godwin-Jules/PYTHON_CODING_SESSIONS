89105f7d44c53247b6851be7a17390665ad4d476
= <=>
2025-06-16 17:28:33 +0000

    Adding data vaidation

--- projects/virtual_doctor/main.py
+++ projects/virtual_doctor/main.py
@@ -80,13 +80,30 @@ def connexion() -> dict[str, str | int | float]:
 
 def createUser() -> dict:
     print("\n\tCREATION D'UN NOUVEAU COMPTE UTILISATEUR\n")
-    nom: str = input("Votre nom : ").upper()
+    nom: str = input("Votre nom : ")
     prenoms: str = input("Votre (Vos) prénom(s) : ")
-    age: int = int(input("Votre âge : "))
+    while True:
+        try:
+            age: int = int(input("Votre âge : "))
+            if age < 0:
+                print("L'âge est toujours supérieur ou égal à 0")
+                continue
+            else:
+                break
+        except Exception as error:
+            print("Saisie incorrecte :", error)
+
     profession: str = input("Votre profession : ")
     email: str = input("Votre adresse email : ")
     country: str = input("Votre pays d'origine : ")
     location: str = input("Votre lieu de résidence : ")
+
+    nom = validateName(nom, "unknown").upper()
+    prenoms = validateName(prenoms, "guest").capitalize()
+    profession = validateName(profession, "unknown").upper()
+    email = validateName(email, "unknown", True).lower()
+    country = validateName(country, "Unknown")
+    location = validateName(location, "Unknown")
 
     user: dict[str, str | int] = {
         "surname": nom,
@@ -101,6 +118,20 @@ def createUser() -> dict:
     return user
 
 
+def validateName(name: str, default: str, email: bool = False) -> str:
+    if len(name) < 3:
+        return default
+    else:
+        if email:
+            return name
+        elif (name.replace(" ", "").isalpha() |
+              name.replace("-", "").isalpha() |
+              name.replace(" ", "").replace("-", "").isalpha()):
+            return name
+        else:
+            return default
+
+
 def getUserInfo() -> tuple[int, float, float]:
     while True:
         while True:
@@ -111,41 +142,41 @@ def getUserInfo() -> tuple[int, float, float]:
                 sexe = input("Votre choix (1 ou 2) : ")
                 sexe = int(sexe)
                 if (sexe not in [1, 2]):
-                    print("Saisie Incorrecte (saisir 1 ou 2)")
+                    print("Saisie Incorrecte (saisir 1 ou 2)\n")
                     continue
                 else:
                     break
             except Exception as e:
-                print("Saisie Incorrecte :", e)
+                print("Saisie Incorrecte :", e, "\n")
 
         while True:
             try:
-                taille = input("\nVeuillez votre taille (en cm) : ")
+                taille = input("Veuillez votre taille (en cm) : ")
                 taille = float(taille)
                 if (taille <= 0):
-                    print("La taille ne peut pas être inférieure ou égale à 0")
+                    print("La taille ne peut pas être inférieure ou égale à 0\n")
                     continue
                 else:
                     break
             except Exception as e:
-                print("Saisie Incorrecte :", e)
+                print("Saisie Incorrecte :", e, "\n")
 
         while True:
             try:
-                poids = input("\nVeuillez votre poids (en Kg) : ")
+                poids = input("Veuillez votre poids (en Kg) : ")
                 poids = float(poids)
                 if (poids <= 0):
-                    print("Le poids ne peut pas être inférieur ou égal à 0")
+                    print("Le poids ne peut pas être inférieur ou égal à 0\n")
                     continue
                 else:
                     break
             except Exception as e:
-                print("Saisie Incorrecte :", e)
+                print("Saisie Incorrecte :", e, "\n")
 
         print("\nINFORMATIONS SAISIES")
-        print(f"* SEXE : {SEX[sexe]}")
-        print(f"* TAILLE : {taille/100}m")
-        print(f"* POIDS : {poids}Kg")
+        print(f"* {"SEXE":8s} :   {SEX[sexe]}")
+        print(f"* {"TAILLE":8s} :   {taille/100}m")
+        print(f"* {"POIDS":8s} :   {poids}Kg")
 
         print("\nVous voulez continuer ou modifier les informations ?")
         valid = input("(o/O) pour modifier, *any pour continuer : ")
@@ -180,6 +211,36 @@ def getQualification(bmi: float) -> int:
         return 0
 
 
+def showUserInfos(user: dict[str, str | int | float]) -> None:
+    qualification: int = user.get("qualification", 0)  # type:ignore
+
+    print("\n\n\tAFFICHAGE DES INFORMATIONS UTILISATEUR")
+    print(f"\t* {"Nom":30s} : {user.get("surname")}")
+    print(f"\t* {"Prénom(s)":30s} : {user.get("names")}")
+    print(f"\t* {"Sexe":30s} : {SEX[user.get("sex")]}")   # type:ignore
+    print(f"\t* {"Âge":30s} : {user.get("age")}")
+    print(f"\t* {"Adresse email":30s} : {user.get("email")}")
+    print(f"\t* {"Profession":30s} : {user.get("profession")}")
+    print(f"\t* {"Pays":30s} : {user.get("country")}")
+    print(f"\t* {"Quartier":30s} : {user.get("location")}")
+
+    print(f"\t* {"Taille":30s} : {user.get("height") / 100}m")    # type:ignore
+    print(f"\t* {"Poids":30s} : {user.get("weight")}Kg")
+    print(f"\t* {"Poids Idéal":30s} : {user.get("ideal_weight")}Kg")
+    print(f"\t* {"IMC (Indce de Masse Corporel)":30s} : {user.get("BMI")}")
+
+    print(f"\t* {"APPRECIATION":30s} : {QUALIFICATIONS[qualification]}\n")
+
+
+def showUserInfo(user: dict[str, str | int | float]) -> None:
+    qualification: int = user.get("qualification", 0)   # type:ignore
+
+    print("\n\n\tRESULTATS DE L'ANALYSE")
+    print(f"\t* Votre poids idéal est : {user.get("ideal_weight")}Kg")
+    print("\t* Votre Indice de Masse Corporel (IMC) est :", user.get("BMI"))
+    print(f"\t* APPRECIATION : {QUALIFICATIONS[qualification]}\n")
+
+
 def updateUser():
     ...
 
@@ -191,11 +252,8 @@ def deleteUser():
 def main() -> None:
     user: dict[str, str | int | float] = connexion()
 
-    print("\n\n\tRESULTATS DE L'ANALYSE")
-    print(f"\t* Votre poids idéal est : {user.get("ideal_weight")}Kg")
-    print("\t* Votre Indice de Masse Corporel (IMC) est :", user.get("BMI"))
-    qualification: int = user.get("qualification", 0)   # type:ignore
-    print(f"\t* APPRECIATION : {QUALIFICATIONS[qualification]}\n")
+    showUserInfo(user)
+    showUserInfos(user)
 
 
 if __name__ == "__main__":
